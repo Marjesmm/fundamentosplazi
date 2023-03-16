fb4d0919300906307ea236f74552f8947b6fb968
MARTINEZ MARTINEZ MARTIN JESUS <mjmartinez@cajapiura.pe>
2023-03-16 11:26:46 -0500

    primer commit

---
+++ src/main/java/fundamentosplazi/springboot/fundamentosplazi/FundamentosplaziApplication.java
@@ -0,0 +1,13 @@
+package fundamentosplazi.springboot.fundamentosplazi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosplaziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentosplaziApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fundamentosplazi/springboot/fundamentosplazi/FundamentosplaziApplicationTests.java
@@ -0,0 +1,13 @@
+package fundamentosplazi.springboot.fundamentosplazi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosplaziApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
